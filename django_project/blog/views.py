7a4b75df8c9be1236ed0f86410739ce15e81b8c8
Lucas Brandi <lgbrandidantas17@gmail.com>
2026-09-02 15:32:44 -0300

    feat: gerando um layout para o blog

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
+posts = [
+    {
+        "author": "Corey Schafer",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "August 27, 2018",
+    },
+]
 
 
 def home(request):
-    return render(request, "blog/home.html")
+    context = {
+        "posts": posts,
+        "title": "Home",
+    }
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return render(request, "blog/about.html")
+    context = {
+        "title": "About",
+    }
+    return render(request, "blog/about.html", context)
